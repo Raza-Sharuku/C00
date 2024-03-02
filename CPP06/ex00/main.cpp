f17d4b24fce6f6aa3859a8371a911e726bca4427
Shahrukh Raza <shahrukh4062@gmail.com>
2024-03-02 15:59:44 +0900

    start to solve CPP06

--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -6,7 +6,19 @@
 /*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 11:24:39 by razasharuku       #+#    #+#             */
-/*   Updated: 2024/03/02 11:24:39 by razasharuku      ###   ########.fr       */
+/*   Updated: 2024/03/02 15:59:06 by razasharuku      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string.h>
+#include <exception>
+
+
+int main(void)
+{
+    int i = 10000 / 10;
+
+    char a = static_cast<char>(i);   
+    std::cout << a << std::endl;
+}
